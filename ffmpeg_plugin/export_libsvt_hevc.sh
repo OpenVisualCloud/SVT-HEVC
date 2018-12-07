e9cd7f81b9500bfce90c1ff9e1ac582bb6db9a1f
Jun Zhao <jun.zhao@intel.com>
2018-12-07 19:12:12 +0800

    FFmpeg Plugin: Update SVT-HEVC FFmpeg plugin based on new build system
    - Add FFmpeg docs's patch
    - Update README.txt based on current SVT-HEVC build system
    - Deleted unsed svt.pc, because the new CMake build will generate the pkg-config file.
    
    Signed-off-by: Jun Zhao <jun.zhao@intel.com>

--- ffmpeg_plugin/export_libsvt_hevc.sh
+++ ffmpeg_plugin/export_libsvt_hevc.sh
@@ -1,5 +1,2 @@
-cp ../Source/API/* /usr/include/
-cp ../Bin/Release/libSvtHevcEnc.so /usr/lib/
-cp svt.pc /usr/lib/pkgconfig/ 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/usr/lib/pkgconfig
