93fef478bb24f6d0f1c42972ffb697a012d74ecb
Jun Zhao <jun.zhao@intel.com>
2018-12-12 09:01:12 +0800

    FFmpeg plugin: Fix SVT-HEVC FFmpeg plugin patch fail issue
    Update SVT-HEVC FFmpeg plugin part and fix the wrong path
    in export_libsvt_hevc.sh for SVT-HEVC plugin.
    
    Note: Now I move the Changelog entry, it's always lead to the merge fail,
    but will add it when we try to upstream the plugin to FFmpeg maillist.
    
    Signed-off-by: Jun Zhao <jun.zhao@intel.com>

--- ffmpeg_plugin/export_libsvt_hevc.sh
+++ ffmpeg_plugin/export_libsvt_hevc.sh
@@ -1,2 +1,2 @@
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/usr/lib/pkgconfig
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/usr/local/lib/pkgconfig
