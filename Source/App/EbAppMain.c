2d14e9220ddcaaf29cd28f9586a2b7cd8d896736
hassount <hassene.tmar@intel.com>
2018-12-13 14:05:34 -0800

    + Revert message in main to show the path of the executable
    Signed-off-by: hassount <hassene.tmar@intel.com>

--- Source/App/EbAppMain.c
+++ Source/App/EbAppMain.c
@@ -305,7 +305,7 @@ int main(int argc, char* argv[])
         }
         else {
             printf("Error in configuration, could not begin encoding! ... \n");
-            printf("Run SvtHevcEncApp -help for a list of options\n");
+            printf("Run %s -help for a list of options\n", argv[0]);
         }
         // Destruct the App memory variables
         for (instanceCount = 0; instanceCount < numChannels; ++instanceCount) {
