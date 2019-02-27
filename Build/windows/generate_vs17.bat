725259692a24ab020cc04fd4d34413c61b739455
Xavier Hallade <xavier.hallade@intel.com>
2019-02-27 11:55:06 +0100

    install Windows version to C:\svt-encoders by default
    CMake's default "C:\Program Files" location breaks pkg-config and tools using it because there is a space in it.

--- Build/windows/generate_vs17.bat
+++ Build/windows/generate_vs17.bat
@@ -1,5 +1,5 @@
 :: Copyright(c) 2018 Intel Corporation 
 :: SPDX-License-Identifier: BSD-2-Clause-Patent
 
-cmake ../.. "-GVisual Studio 15 2017 Win64" -DCMAKE_ASM_NASM_COMPILER="yasm.exe" -DCMAKE_CONFIGURATION_TYPES="Debug;Release"
+cmake ../.. "-GVisual Studio 15 2017 Win64" -DCMAKE_ASM_NASM_COMPILER="yasm.exe" -DCMAKE_INSTALL_PREFIX=%SYSTEMDRIVE%\svt-encoders -DCMAKE_CONFIGURATION_TYPES="Debug;Release"
 pause
