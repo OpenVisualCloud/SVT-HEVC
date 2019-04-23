1f967989a0645b8f9e4eb2bbbeb54d93a6b9a9ef
Christopher Degawa <ccom@randomderp.com>
2019-04-23 16:22:09 -0500

    Rename batch to build.bat, Update readme and fix formmating

--- Build/windows/build.bat
+++ Build/windows/build.bat
@@ -14,71 +14,75 @@ goto :EOF
 :args
 if -%1-==-- (
     goto argend
-) else if "%1"=="/help" (
+) else if /I "%1"=="/help" (
     call :help
-) else if "%1"=="help" (
+) else if /I "%1"=="help" (
     call :help
-) else if "%1"=="clean" (
+) else if /I "%1"=="clean" (
     echo Cleaning build folder
-    for %%i in (*) do if not "%%~i" == "generate.bat" del "%%~i"
-    for /d %%i in (*) do if not "%%~i" == "generate.bat" (
+    for %%i in (*) do if not "%%~i" == "build.bat" del "%%~i"
+    for /d %%i in (*) do if not "%%~i" == "build.bat" (
         del /f /s /q "%%~i" 1>nul
         rmdir /s /q "%%~i" 1>nul
     )
     exit
-) else if "%1"=="2019" (
+) else if /I "%1"=="2019" (
     echo Building for Visual Studio 2019
     set "GENERATOR=Visual Studio 16 2019"
     shift
-) else if "%1"=="2017" (
+) else if /I "%1"=="2017" (
     echo Building for Visual Studio 2017
     set "GENERATOR=Visual Studio 15 2017 Win64"
     shift
-) else if "%1"=="2015" (
+) else if /I "%1"=="2015" (
     echo Building for Visual Studio 2015
     set "GENERATOR=Visual Studio 14 2015 Win64"
     shift
-) else if "%1"=="2013" (
+) else if /I "%1"=="2013" (
     echo Building for Visual Studio 2013
+    echo This is currently not officially supported
     set "GENERATOR=Visual Studio 12 2013 Win64"
     shift
-) else if "%1"=="2012" (
+) else if /I "%1"=="2012" (
     echo Building for Visual Studio 2012
+    echo This is currently not officially supported
     set "GENERATOR=Visual Studio 11 2012 Win64"
     shift
-) else if "%1"=="2010" (
+) else if /I "%1"=="2010" (
     echo Building for Visual Studio 2010
+    echo This is currently not officially supported
     set "GENERATOR=Visual Studio 10 2010 Win64"
     shift
-) else if "%1"=="2008" (
+) else if /I "%1"=="2008" (
     echo Building for Visual Studio 2008
+    echo This is currently not officially supported
     set "GENERATOR=Visual Studio 9 2008 Win64"
     shift
-) else if "%1"=="ninja" (
+) else if /I "%1"=="ninja" (
     echo Building for Ninja files
     echo This is currently not officially supported
     set "GENERATOR=Ninja"
     shift
-) else if "%1"=="msys" (
+) else if /I "%1"=="msys" (
     echo Building for MSYS Makefiles
     echo This is currently not officially supported
     set "GENERATOR=MSYS Makefiles"
     shift
-) else if "%1"=="mingw" (
+) else if /I "%1"=="mingw" (
     echo Building for MinGW Makefiles
     echo This is currently not officially supported
     set "GENERATOR=MinGW Makefiles"
     shift
-) else if "%1"=="unix" (
+) else if /I "%1"=="unix" (
     echo Building for Unix Makefiles
     echo This is currently not officially supported
     set "GENERATOR=Unix Makefiles"
     shift
-) else if "%1"=="release" (
+) else if /I "%1"=="release" (
     echo Building for release
     set "buildtype=--config release"
     shift
-) else if "%1"=="debug" (
+) else if /I "%1"=="debug" (
     echo Building for debug
     set "buildtype= --config debug"
     shift
@@ -102,7 +106,7 @@ cmake --build . %buildtype%
 goto :EOF
 
 :help
-    echo Batch file to generate project files for building SVT-AV1 on Windows
-    echo Usage: generate.bat 2019^|2017^|2015^|clean
+    echo Batch file to build SVT-HEVC on Windows
+    echo Usage: generate.bat 2019^|2017^|2015^|clean [release|debug]
     exit
 goto :EOF
