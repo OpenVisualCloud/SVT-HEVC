85a36e2ddd90e2fc19ad34e82f351b487a50f9fa
kirithika <kirithika@multicorewareinc.com>
2019-07-30 18:53:38 +0530

    Fix license header in newly added files
    Signed-off-by: kirithika <kirithika@multicorewareinc.com>

--- Source/Lib/ASM_AVX2/const-a.asm
+++ Source/Lib/ASM_AVX2/const-a.asm
@@ -1,30 +1,7 @@
-;*****************************************************************************
-;* const-a.asm: x86 global constants
-;*****************************************************************************
-;* Copyright (C) 2003-2013 x264 project
-;* Copyright (C) 2013-2017 MulticoreWare, Inc
-;*
-;* Authors: Loren Merritt <lorenm@u.washington.edu>
-;*          Fiona Glaser <fiona@x264.com>
-;*          Min Chen <chenm003@163.com> <min.chen@multicorewareinc.com>
-;*          Praveen Kumar Tiwari <praveen@multicorewareinc.com>
-;* This program is free software; you can redistribute it and/or modify
-;* it under the terms of the GNU General Public License as published by
-;* the Free Software Foundation; either version 2 of the License, or
-;* (at your option) any later version.
-;*
-;* This program is distributed in the hope that it will be useful,
-;* but WITHOUT ANY WARRANTY; without even the implied warranty of
-;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;* GNU General Public License for more details.
-;*
-;* You should have received a copy of the GNU General Public License
-;* along with this program; if not, write to the Free Software
-;* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
-;*
-;* This program is also available under a commercial proprietary license.
-;* For more information, contact us at license @ x265.com.
-;*****************************************************************************
+; 
+; Copyright(c) 2018 Intel Corporation
+; SPDX - License - Identifier: BSD - 2 - Clause - Patent
+; 
 
 %include "x86inc.asm"
 
