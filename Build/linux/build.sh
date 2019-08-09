d808e90b3cbffdaf3562309e47374c94c2a77e17
Mark Feldman <mark.feldman@intel.com>
2019-08-09 07:09:17 -0700

    Replacing tabs in build.sh
    Replacing tabs in build.sh using
     ls -Recurse -File -Filter *.sh | ForEach-Object{$(Get-Content
    $_.FullName | Foreach {Write-Output "$($_.TrimEnd().Replace("`t","    "))
    `n"}) | Set-Content -NoNewline -Encoding utf8 $_.FullName}

--- Build/linux/build.sh
+++ Build/linux/build.sh
@@ -1,47 +1,47 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 # Copyright(c) 2018 Intel Corporation
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 
 function clean {
-	rm -R -f debug
-	rm -R -f release
+    rm -R -f debug
+    rm -R -f release
     rm -R -f ../../Bin/Debug
     rm -R -f ../../Bin/Release
 }
 
 function debug {
-	mkdir -p debug
-	mkdir -p ../../Bin/Debug
-	cd debug
-	PATH=$PATH:/usr/local/bin/
-	cmake ../../.. 					\
-		-DCMAKE_BUILD_TYPE=Debug			\
-		-DCMAKE_C_COMPILER=$CMAKE_COMPILER		\
-		-DCMAKE_ASM_NASM_COMPILER=$CMAKE_ASSEMBLER	\
-		-DCMAKE_AR=`which $AR_COMPILER`			\
-		-DCMAKE_RANLIB=`which $RANLIB_COMPILER`	\
+    mkdir -p debug
+    mkdir -p ../../Bin/Debug
+    cd debug
+    PATH=$PATH:/usr/local/bin/
+    cmake ../../..                     \
+        -DCMAKE_BUILD_TYPE=Debug            \
+        -DCMAKE_C_COMPILER=$CMAKE_COMPILER        \
+        -DCMAKE_ASM_NASM_COMPILER=$CMAKE_ASSEMBLER    \
+        -DCMAKE_AR=`which $AR_COMPILER`            \
+        -DCMAKE_RANLIB=`which $RANLIB_COMPILER`    \
 
-	# Compile the App
-	make -j SvtHevcEncApp
-	cd ..
+    # Compile the App
+    make -j SvtHevcEncApp
+    cd ..
 }
 
 function release {
-	mkdir -p release
-	mkdir -p ../../Bin/Release
-	cd release
-	PATH=$PATH:/usr/local/bin/
-	cmake ../../.. 					\
-		-DCMAKE_BUILD_TYPE=Release			\
-		-DCMAKE_C_COMPILER=$CMAKE_COMPILER		\
-		-DCMAKE_ASM_NASM_COMPILER=$CMAKE_ASSEMBLER	\
-		-DCMAKE_AR=`which $AR_COMPILER`			\
-		-DCMAKE_RANLIB=`which $RANLIB_COMPILER`	\
+    mkdir -p release
+    mkdir -p ../../Bin/Release
+    cd release
+    PATH=$PATH:/usr/local/bin/
+    cmake ../../..                     \
+        -DCMAKE_BUILD_TYPE=Release            \
+        -DCMAKE_C_COMPILER=$CMAKE_COMPILER        \
+        -DCMAKE_ASM_NASM_COMPILER=$CMAKE_ASSEMBLER    \
+        -DCMAKE_AR=`which $AR_COMPILER`            \
+        -DCMAKE_RANLIB=`which $RANLIB_COMPILER`    \
 
-	# Compile the App
-	make -j SvtHevcEncApp
-	cd ..
+    # Compile the App
+    make -j SvtHevcEncApp
+    cd ..
 }
 
 # Defines
@@ -52,20 +52,20 @@ RANLIB_COMPILER=gcc-ranlib
 CMAKE_COMPILER=$GCC_COMPILER
 
 if [ $# -eq 0 ]; then
-	release
+    release
 elif [ "$1" = "clean" ]; then
-	clean
+    clean
 elif [ "$1" = "debug" ]; then
-	debug
+    debug
 elif [ "$1" = "release" ]; then
-	release
+    release
 
 elif [ "$1" = "all" ]; then
-	release
+    release
 elif [ "$1" = "gcc" ]; then
-	release
+    release
 else
-	echo "build.sh <clean|all|debug|release|help>"
+    echo "build.sh <clean|all|debug|release|help>"
 fi
 
 exit
