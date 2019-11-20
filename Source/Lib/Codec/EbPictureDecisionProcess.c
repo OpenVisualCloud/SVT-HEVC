OpenVisualCloud/SVT-HEVC

commit e69c96544c2142ba03cea80de097a91caecd7b67

Add VPS/SPS/PPS insertion for IDR frames option (#250)
Note: IDR headers insertion has limitation for CQP. SCD might change any next frame to intra, and then it resets the GOP's beginning position if the RC mode is CQP. While if VBR, no such reset.

Signed-off-by: Jing Sun <jing.a.sun@intel.com>

Source/Lib/Codec/EbPictureDecisionProcess.c
@@ -954,28 +954,23 @@ void* PictureDecisionKernel(void *inputPtr)
             } 
             pictureControlSetPtr                        = (PictureParentControlSet_t*)  queueEntryPtr->parentPcsWrapperPtr->objectPtr;
 
-           if(pictureControlSetPtr->idrFlag == EB_TRUE)
-                 contextPtr->lastSolidColorFramePoc = 0xFFFFFFFF;
+            if(pictureControlSetPtr->idrFlag == EB_TRUE)
+                contextPtr->lastSolidColorFramePoc = 0xFFFFFFFF;
 
-			if(windowAvail == EB_TRUE){
-				if(sequenceControlSetPtr->staticConfig.sceneChangeDetection) {
+            if(windowAvail == EB_TRUE) {
+                if(sequenceControlSetPtr->staticConfig.sceneChangeDetection) {
                     pictureControlSetPtr->sceneChangeFlag = SceneTransitionDetector(
                         contextPtr,
                         sequenceControlSetPtr,
                         ParentPcsWindow,
                         FUTURE_WINDOW_WIDTH);
-
                 } else {
-                    pictureControlSetPtr->sceneChangeFlag           = EB_FALSE;
+                    pictureControlSetPtr->sceneChangeFlag = EB_FALSE;
                 }
-				pictureControlSetPtr->craFlag = (pictureControlSetPtr->sceneChangeFlag == EB_TRUE) ? 
-					EB_TRUE : 
-					pictureControlSetPtr->craFlag;
 
-				// Store scene change in context
-				contextPtr->isSceneChangeDetected = pictureControlSetPtr->sceneChangeFlag;
-
-			}
+                // Store scene change in context
+                contextPtr->isSceneChangeDetected = pictureControlSetPtr->sceneChangeFlag;
+            }
 
             if(windowAvail == EB_TRUE ||framePasseThru == EB_TRUE)
 			{
@@ -1003,26 +998,25 @@ void* PictureDecisionKernel(void *inputPtr)
            ReleasePrevPictureFromReorderQueue(
                encodeContextPtr);
 
+            pictureControlSetPtr->craFlag = EB_FALSE;
+            pictureControlSetPtr->idrFlag = EB_FALSE;
+
             // If the Intra period length is 0, then introduce an intra for every picture
-            if(sequenceControlSetPtr->intraPeriodLength == 0) {
-                pictureControlSetPtr->craFlag = EB_TRUE;
+            if ((sequenceControlSetPtr->intraPeriodLength == 0) || (pictureControlSetPtr->pictureNumber == 0)) {
+                if (sequenceControlSetPtr->intraRefreshType == CRA_REFRESH)
+                    pictureControlSetPtr->craFlag = EB_TRUE;
+                else
+                    pictureControlSetPtr->idrFlag = EB_TRUE;
             }
             // If an #IntraPeriodLength has passed since the last Intra, then introduce a CRA or IDR based on Intra Refresh type
-            else if(sequenceControlSetPtr->intraPeriodLength != -1) {
-                pictureControlSetPtr->craFlag = 
-                    (sequenceControlSetPtr->intraRefreshType != CRA_REFRESH) ?
-                        pictureControlSetPtr->craFlag :
-                        (encodeContextPtr->intraPeriodPosition == (EB_U32) sequenceControlSetPtr->intraPeriodLength) ? 
-                            EB_TRUE : 
-                            pictureControlSetPtr->craFlag;
-
-                pictureControlSetPtr->idrFlag = 
-                    (sequenceControlSetPtr->intraRefreshType != IDR_REFRESH) ? 
-                        pictureControlSetPtr->idrFlag :
-                        (encodeContextPtr->intraPeriodPosition == (EB_U32) sequenceControlSetPtr->intraPeriodLength) ? 
-                            EB_TRUE : 
-                            pictureControlSetPtr->idrFlag;
-                
+            else if (sequenceControlSetPtr->intraPeriodLength != -1) {
+                if ((encodeContextPtr->intraPeriodPosition == (EB_U32) sequenceControlSetPtr->intraPeriodLength) ||
+                    (pictureControlSetPtr->sceneChangeFlag == EB_TRUE)) {
+                    if (sequenceControlSetPtr->intraRefreshType == CRA_REFRESH)
+                        pictureControlSetPtr->craFlag = EB_TRUE;
+                    else
+                        pictureControlSetPtr->idrFlag = EB_TRUE;
+                }
             }
 
             encodeContextPtr->preAssignmentBufferEosFlag             = (pictureControlSetPtr->endOfSequenceFlag) ? (EB_U32)EB_TRUE : encodeContextPtr->preAssignmentBufferEosFlag;
@@ -1032,16 +1026,17 @@ void* PictureDecisionKernel(void *inputPtr)
             encodeContextPtr->preAssignmentBufferIdrCount           += pictureControlSetPtr->idrFlag;
             encodeContextPtr->preAssignmentBufferCount              += 1;
 
-			if (sequenceControlSetPtr->staticConfig.rateControlMode)
-			{
-				// Increment the Intra Period Position
-				encodeContextPtr->intraPeriodPosition = (encodeContextPtr->intraPeriodPosition == (EB_U32)sequenceControlSetPtr->intraPeriodLength) ? 0 : encodeContextPtr->intraPeriodPosition + 1;
-			}
-			else
-			{
-				// Increment the Intra Period Position
-				encodeContextPtr->intraPeriodPosition = ((encodeContextPtr->intraPeriodPosition == (EB_U32)sequenceControlSetPtr->intraPeriodLength) || (pictureControlSetPtr->sceneChangeFlag == EB_TRUE)) ? 0 : encodeContextPtr->intraPeriodPosition + 1;
-			}
+            if (sequenceControlSetPtr->staticConfig.rateControlMode)
+            {
+                // Increment the Intra Period Position
+                encodeContextPtr->intraPeriodPosition = (encodeContextPtr->intraPeriodPosition == (EB_U32)sequenceControlSetPtr->intraPeriodLength) ?
+                                                        0 : encodeContextPtr->intraPeriodPosition + 1;
+            } else {
+                // Increment the Intra Period Position
+                encodeContextPtr->intraPeriodPosition = ((encodeContextPtr->intraPeriodPosition == (EB_U32)sequenceControlSetPtr->intraPeriodLength) ||
+                                                        (pictureControlSetPtr->sceneChangeFlag == EB_TRUE)) ?
+                                                        0 : encodeContextPtr->intraPeriodPosition + 1;
+            }
 
 			// Determine if Pictures can be released from the Pre-Assignment Buffer
 			if ((encodeContextPtr->preAssignmentBufferIntraCount > 0) ||

Config/Sample.cfg
@@ -53,7 +53,7 @@ HierarchicalLevels              : 3                     # Minigop Size = (2^Hier
 BaseLayerSwitchMode             : 0                     # 0 : Use B-frames in the base layer pointing to the same past picture, 1 : Use P-frames in the base layer
 PredStructure                   : 2				        # 0: Low Delay P, 1: Low Delay B, 2: Random Access
 IntraPeriod                     : -2                    # Period of I-Frame (-1 = only first, -2 = auto) [-2 - 255]
-IntraRefreshType                : 1                     # Random Accesss 1:CRA, 2:IDR (when IntraPeriod > 0) - [1-2]
+IntraRefreshType                : -1                    # Random Accesss -1:CRA, >=0:IDR (0 supported if CQP, >=0 supported if VBR)
 
 # ====================== Quantization ===============================
 QP                              : 32                    # Quantization parameter - [0-51]

Source/API/EbApi.h
@@ -180,11 +180,11 @@ typedef struct EB_H265_ENC_CONFIGURATION
 
     /* Random access.
      *
-     * 1 = CRA, open GOP.
-     * 2 = IDR, closed GOP.
+     * -1  = CRA, open GOP.
+     * >=0 = IDR, closed GOP, and the value is headers insertion interval.
      *
-     * Default is 1. */
-    uint32_t                intraRefreshType;
+     * Default is -1. */
+    int32_t                intraRefreshType;
 
     /* Number of hierarchical layers used to construct GOP.
      * Minigop size = 2^HierarchicalLevels.

Source/App/EbAppConfig.c
@@ -502,7 +502,7 @@ void EbConfigCtor(EbConfig_t *configPtr)
     configPtr->baseLayerSwitchMode                  = 0;
     configPtr->predStructure                        = 2;
     configPtr->intraPeriod                          = -2;
-    configPtr->intraRefreshType                     = 1;
+    configPtr->intraRefreshType                     = 0;
 
     // DLF
     configPtr->disableDlfFlag                       = EB_FALSE;

Source/App/EbAppConfig.h
@@ -274,8 +274,8 @@ typedef struct EbConfig_s
      *****************************************/
     uint32_t                 baseLayerSwitchMode;
     uint8_t                  encMode;
-    int32_t                 intraPeriod;
-    uint32_t                 intraRefreshType;
+    int32_t                  intraPeriod;
+    int32_t                  intraRefreshType;
 	uint32_t                 hierarchicalLevels;
 	uint32_t                 predStructure;
 

Source/App/EbAppContext.c
@@ -164,8 +164,15 @@ EB_ERRORTYPE CopyConfigurationParameters(
     callbackData->ebEncParameters.sourceWidth = config->sourceWidth;
     callbackData->ebEncParameters.sourceHeight = config->sourceHeight;
     callbackData->ebEncParameters.interlacedVideo = (EB_BOOL)config->interlacedVideo;
+    callbackData->ebEncParameters.rateControlMode = config->rateControlMode;
     callbackData->ebEncParameters.intraPeriodLength = config->intraPeriod;
     callbackData->ebEncParameters.intraRefreshType = config->intraRefreshType;
+    if (config->rateControlMode == 0 && config->intraRefreshType > 0)
+    {
+        printf("\nWarning: intraRefreshType >0 is only supported in VBR mode\n");
+        callbackData->ebEncParameters.intraRefreshType = 0;
+    }
+
     callbackData->ebEncParameters.baseLayerSwitchMode = config->baseLayerSwitchMode;
     callbackData->ebEncParameters.encMode = (EB_BOOL)config->encMode;
     callbackData->ebEncParameters.frameRate = config->frameRate;
@@ -176,7 +183,6 @@ EB_ERRORTYPE CopyConfigurationParameters(
     callbackData->ebEncParameters.sceneChangeDetection = config->sceneChangeDetection;
     callbackData->ebEncParameters.lookAheadDistance = config->lookAheadDistance;
     callbackData->ebEncParameters.framesToBeEncoded = config->framesToBeEncoded;
-    callbackData->ebEncParameters.rateControlMode = config->rateControlMode;
     callbackData->ebEncParameters.targetBitRate = config->targetBitRate;
     callbackData->ebEncParameters.maxQpAllowed = config->maxQpAllowed;
     callbackData->ebEncParameters.minQpAllowed = config->minQpAllowed;

Source/Lib/Codec/EbDefinitions.h
@@ -488,9 +488,8 @@ typedef struct EB_H265_DYN_ENC_CONFIGURATION
 /** The EB_INTRA_REFRESH_TYPE is used to describe the intra refresh type.
 */
 typedef enum EB_INTRA_REFRESH_TYPE {
-    NO_REFRESH = 0,
-    CRA_REFRESH = 1,
-    IDR_REFRESH = 2
+    CRA_REFRESH = -1,
+    IDR_REFRESH = 0
 }EB_INTRA_REFRESH_TYPE;
 
 #if defined(_MSC_VER)

Source/Lib/Codec/EbEncHandle.c
@@ -2171,7 +2171,7 @@ static EB_S32 ComputeIntraPeriod(
 
     intraPeriod = (ABS((fps - maxIp)) > ABS((fps - minIp))) ? minIp : maxIp;
 
-    if(config->intraRefreshType == 1)
+    if (config->intraRefreshType == CRA_REFRESH)
         intraPeriod -= 1;
 
     return intraPeriod;
@@ -2741,10 +2741,11 @@ static EB_ERRORTYPE VerifySettings(\
         return_error = EB_ErrorBadParameter;
     }
 
-    if( config->intraRefreshType > 2 || config->intraRefreshType < 1) {
-        SVT_LOG("SVT [Error]: Instance %u: Invalid intra Refresh Type [1-2]\n",channelNumber+1);
+    if (config->intraRefreshType < CRA_REFRESH) {
+        SVT_LOG("SVT [Error]: Instance %u: Intra refresh type must be -1 (CRA) or >=0 (IDR)\n",channelNumber+1);
         return_error = EB_ErrorBadParameter;
-	}
+    }
+
     if (config->baseLayerSwitchMode > 1) {
         SVT_LOG("SVT [Error]: Instance %u: Invalid Base Layer Switch Mode [0-1] \n",channelNumber+1);
         return_error = EB_ErrorBadParameter;
@@ -3135,7 +3136,7 @@ EB_ERRORTYPE EbH265EncInitParameter(
     configPtr->baseLayerSwitchMode = 0;
     configPtr->encMode  = 7;
     configPtr->intraPeriodLength = -2;
-    configPtr->intraRefreshType = 1;
+    configPtr->intraRefreshType = CRA_REFRESH;
     configPtr->hierarchicalLevels = 3;
     configPtr->predStructure = EB_PRED_RANDOM_ACCESS;
     configPtr->disableDlfFlag = EB_FALSE;

commit 1c03356d8249ce36a87988708d3a47cec8cd79a4

Fixed the IDR insertion failure regression.
The regression was caused by PR #250. So fixed it by rolling back to
the original logic to set IDR (and CRA) flag.

Signed-off-by: Austin Hu <austin.hu@intel.com>

Source/Lib/Codec/EbPictureDecisionProcess.c
@@ -738,9 +738,6 @@ void* PictureDecisionKernel(void *inputPtr)
            ReleasePrevPictureFromReorderQueue(
                encodeContextPtr);
 
-            pictureControlSetPtr->craFlag = EB_FALSE;
-            pictureControlSetPtr->idrFlag = EB_FALSE;
-
             // If the Intra period length is 0, then introduce an intra for every picture
             if ((sequenceControlSetPtr->intraPeriodLength == 0) || (pictureControlSetPtr->pictureNumber == 0)) {
                 if (sequenceControlSetPtr->intraRefreshType == CRA_REFRESH)