843f1607ca944f41fd617b69f7c15765cf66ad73
Austin Hu <austin.hu@intel.com>
2020-02-13 15:57:39 -0700

    Fixed the induced random seg fault issue due to refPaPicPtrArray.
    Signed-off-by: Austin Hu <austin.hu@intel.com>

--- Source/Lib/Codec/EbEncDecProcess.c
+++ Source/Lib/Codec/EbEncDecProcess.c
@@ -3054,14 +3054,14 @@ void* EncDecKernel(void *inputPtr)
             if (((pictureControlSetPtr->sliceType == EB_P_PICTURE) || (pictureControlSetPtr->sliceType == EB_B_PICTURE)) &&
                     pictureControlSetPtr->ParentPcsPtr->refPaPcsArray[REF_LIST_0]) {
                 ((EbPaReferenceObject_t *)pictureControlSetPtr->ParentPcsPtr->refPaPcsArray[REF_LIST_0]->paReferencePictureWrapperPtr->objectPtr)->dependentPicturesCount--;
-                EbReleaseObject(pictureControlSetPtr->ParentPcsPtr->refPaPicPtrArray[REF_LIST_0]);
+                EbReleaseObject(pictureControlSetPtr->ParentPcsPtr->refPaPcsArray[REF_LIST_0]->paReferencePictureWrapperPtr);
                 EbReleaseObject(pictureControlSetPtr->ParentPcsPtr->refPaPcsArray[REF_LIST_0]->pPcsWrapperPtr);
             }
 
             if ((pictureControlSetPtr->sliceType == EB_B_PICTURE) &&
                     pictureControlSetPtr->ParentPcsPtr->refPaPcsArray[REF_LIST_1]) {
                 ((EbPaReferenceObject_t *)pictureControlSetPtr->ParentPcsPtr->refPaPcsArray[REF_LIST_1]->paReferencePictureWrapperPtr->objectPtr)->dependentPicturesCount--;
-                EbReleaseObject(pictureControlSetPtr->ParentPcsPtr->refPaPicPtrArray[REF_LIST_1]);
+                EbReleaseObject(pictureControlSetPtr->ParentPcsPtr->refPaPcsArray[REF_LIST_1]->paReferencePictureWrapperPtr);
                 EbReleaseObject(pictureControlSetPtr->ParentPcsPtr->refPaPcsArray[REF_LIST_1]->pPcsWrapperPtr);
             }
 
